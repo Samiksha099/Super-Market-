888780cb83c4bd42d561827df896e5a1147fd9d5
Samiksha099 <83509192+Samiksha099@users.noreply.github.com>
2021-08-24 01:27:38 +0530

    Add files via upload

---
+++ SuperMarket/src/main/java/com/market/repository/CartRepo.java
@@ -0,0 +1,9 @@
+package com.market.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.market.entity.Cart;
+
+public interface CartRepo extends JpaRepository<Cart, Integer>{
+
+}
